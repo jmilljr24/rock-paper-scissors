fcf8cd66d82f80be3c9022af604ea6b8dcfc0023
jmilljr24 <jmilljr24@gmail.com>
2022-11-17 15:10:33 -0500

    Add function to play game of five rounds
    The function plays five rounds while outputting the cumulative score until the end and outputs a winner

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 /*getComputerChoice with options rock paper or scissors randomly*/
 
 const choices = ["rock", "paper", "scissors"];
-const random = Math.floor(Math.random() * choices.length);
-
+function computerRandomSelection() {
+  const random = Math.floor(Math.random() * choices.length);
+  return choices[random];
+}
 /*get playerSelection */
-let userInput = window.prompt("Rock, Paper or Scissors?");
 
 /*get computerSelection */
-const computerChoice = choices[random];
+/*/const computerChoice = choices[random];*/
 
-console.log(userInput);
-console.log(computerChoice);
+/*/console.log(userInput);*/
+/*console.log(computerChoice);*/
 /*function to compare choices and return winner*/
 function playRound(playerSelection, computerSelection) {
   if (
     playerSelection.toLowerCase() === "rock" &&
     computerSelection === "paper"
   ) {
+    computerScore++;
     return "You Lose! Paper beats Rock";
   } else if (
     playerSelection.toLowerCase() === "rock" &&
     computerSelection === "scissors"
   ) {
+    userScore++;
     return "You Win! Rock beats Scissors";
   } else if (
     playerSelection.toLowerCase() === "rock" &&
@@ -37,16 +40,19 @@ function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase() === "paper" &&
     computerSelection === "scissors"
   ) {
+    computerScore++;
     return "You Lose! Scissors beat Paper";
   } else if (
     playerSelection.toLowerCase() === "paper" &&
     computerSelection === "rock"
   ) {
+    userScore++;
     return "You Win! Paper beats Rock";
   } else if (
     playerSelection.toLowerCase() === "scissors" &&
     computerSelection === "paper"
   ) {
+    userScore++;
     return "You Win! Scissors beat Paper";
   } else if (
     playerSelection.toLowerCase() === "scissors" &&
@@ -57,9 +63,29 @@ function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase() === "scissors" &&
     computerSelection === "rock"
   ) {
+    computerScore++;
     return "You Lose! Rock beats Paper";
   } else return "check your spelling";
 }
-console.log(playRound(userInput, computerChoice));
 
 /*Call playRound function for five rounds and keep score. report winner*/
+
+let userScore = 0;
+let computerScore = 0;
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let userInput = window.prompt("Rock, Paper or Scissors?");
+    let computerChoice = computerRandomSelection();
+    console.log(playRound(userInput, computerChoice));
+    console.log("Computer", computerScore);
+    console.log("Me", userScore);
+  }
+  if (userScore > computerScore) {
+    console.log("You are the Winner!!");
+  } else if (userScore < computerScore) {
+    console.log("You lost! Try again");
+  } else console.log("It's a draw!");
+}
+
+game();
