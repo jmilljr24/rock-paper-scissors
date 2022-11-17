a5b8a846db502a578fcfcd492e50f59916ab041e
jmilljr24 <jmilljr24@gmail.com>
2022-11-17 12:06:22 -0500

    Add script.js with random getComputerChoice

---
+++ script.js
@@ -0,0 +1,11 @@
+/*getComputerChoice with options rock paper or scissors randomly*/
+
+const choices = ["rock", "paper", "scissors"];
+const random = Math.floor(Math.random() * choices.length);
+
+let getComputerChoice = choices[random];
+console.log(getComputerChoice);
+
+/*get playerSelection */
+
+/*get computerSelection */
